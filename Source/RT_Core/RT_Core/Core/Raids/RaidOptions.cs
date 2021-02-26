78e3b2a52a95037f9da83e7771d60981ee06f032
Taranchuk <nfhfyxer@gmail.com>
2021-02-26 19:37:19 +0200

    update

---
+++ Source/RT_Core/RT_Core/Core/Raids/RaidOptions.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RT_Core
+{
+    public class RaidOptions : DefModExtension
+    {
+        public PawnGroupMaker pawnGroup;
+        public int minimumPawnCount;
+        public int fixedRaidPoints = -1;
+        public float raidPointsMultiplier = -1f;
+        public FactionDef raidFaction;
+        public RaidStrategyDef raidStrategy;
+        public PawnsArrivalModeDef raidArrival;
+    }
+}
