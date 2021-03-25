7a41809a88b5a29bc3b195bdb39fe6bdd2cd1255
Taranchuk <nfhfyxer@gmail.com>
2021-03-25 23:38:39 +0200

    update

--- Source/RT_Core/RT_Core/Core/AbsorbEnergy/Ability_AbsorbEnergy.cs
+++ Source/RT_Core/RT_Core/Core/AbsorbEnergy/Ability_AbsorbEnergy.cs
@@ -17,7 +17,7 @@ namespace RT_Core
 
         public override bool Activate(LocalTargetInfo target, LocalTargetInfo dest)
         {
-            if (target.Pawn.BodySize <= 4f && !Utils.blackListRaces.Contains(target.Thing.def))
+            if (target.Pawn?.BodySize <= 4f && !Utils.blackListRaces.Contains(target.Thing.def))
             {
                 var hediff = HediffMaker.MakeHediff(RT_DefOf.RT_MetroidHunting, pawn);
                 pawn.health.AddHediff(hediff);
