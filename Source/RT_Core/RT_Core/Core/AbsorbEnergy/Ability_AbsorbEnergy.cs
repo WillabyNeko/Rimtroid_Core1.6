0733c0d142e924656df20541337d4b4ac429673f
Taranchuk <nfhfyxer@gmail.com>
2021-03-25 23:52:07 +0200

    update

--- Source/RT_Core/RT_Core/Core/AbsorbEnergy/Ability_AbsorbEnergy.cs
+++ Source/RT_Core/RT_Core/Core/AbsorbEnergy/Ability_AbsorbEnergy.cs
@@ -15,18 +15,55 @@ namespace RT_Core
         public Ability_AbsorbEnergy(Pawn pawn) : base(pawn) { }
         public Ability_AbsorbEnergy(Pawn pawn, AbilityDef def) : base(pawn, def) { }
 
+        public override bool CanApplyOn(LocalTargetInfo target)
+        {
+            Predicate<LocalTargetInfo> validator = delegate (LocalTargetInfo targetInfo)
+            {
+                if (target.Thing is Corpse corpse)
+                {
+                    return !Utils.blackListRaces.Contains(corpse.InnerPawn.def)
+                    && corpse.GetRotStage() == RotStage.Fresh && corpse.Age < GenDate.TicksPerDay * 3 && pawn.CanReserve(corpse);
+                }
+                else if (target.Thing is Pawn victim && victim.IsPrisoner && !victim.Downed && victim.GetComp<CompPrisonerFeed>().canBeEaten && pawn.CanReserve(victim))
+                {
+                    return true;
+                }
+                else if (target.Thing is Pawn victim2 && (pawn.Faction is null && !victim2.IsAnyMetroid() || pawn.Faction == Faction.OfPlayer) && !Utils.blackListRaces.Contains(victim2.def) 
+                && victim2.RaceProps.Animal && victim2.Faction != pawn.Faction && victim2.BodySize <= 4f && pawn.CanReserve(victim2))
+                {
+                    return true;
+                }
+                return false;
+            };
+            if (validator(target))
+            {
+                return true;
+            }
+            return false;
+        }
         public override bool Activate(LocalTargetInfo target, LocalTargetInfo dest)
         {
-            if (target.Pawn?.BodySize <= 4f && !Utils.blackListRaces.Contains(target.Thing.def))
+            if (target.Thing is Corpse corpse && !Utils.blackListRaces.Contains(corpse.InnerPawn.def)
+                && corpse.GetRotStage() == RotStage.Fresh && corpse.Age < GenDate.TicksPerDay * 3 && pawn.CanReserve(corpse))
             {
                 var hediff = HediffMaker.MakeHediff(RT_DefOf.RT_MetroidHunting, pawn);
                 pawn.health.AddHediff(hediff);
                 var job = JobMaker.MakeJob(RT_DefOf.RT_AbsorbingEnergy, target);
                 pawn.jobs.TryTakeOrderedJob(job);
             }
-            else
+            else if (target.Pawn != null)
             {
-                Messages.Message("RT.AnimalIsTooBig".Translate(target.Pawn.LabelCap), MessageTypeDefOf.CautionInput);
+                if (target.Pawn?.BodySize <= 4f && !Utils.blackListRaces.Contains(target.Thing.def))
+                {
+                    var hediff = HediffMaker.MakeHediff(RT_DefOf.RT_MetroidHunting, pawn);
+                    pawn.health.AddHediff(hediff);
+                    var job = JobMaker.MakeJob(RT_DefOf.RT_AbsorbingEnergy, target);
+                    pawn.jobs.TryTakeOrderedJob(job);
+                }
+                else
+                {
+                    Messages.Message("RT.AnimalIsTooBig".Translate(target.Pawn.LabelCap), MessageTypeDefOf.CautionInput);
+                }
             }
             return base.Activate(target, dest);
         }
